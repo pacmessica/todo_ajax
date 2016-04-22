fa622ba9742b36a1eab130b9ea62eb83dab2891f
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-22 09:44:54 +0200

    fix controller

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,7 +5,7 @@ class TodosController < ApplicationController
   # GET /todos.json
   def index
     @todo = Todo.new
-    @todos = Todo.all
+    @todos = Todo.all.order(:id)
   end
 
   # GET /todos/1
